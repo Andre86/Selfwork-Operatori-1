691afba1e399530e434b43488eaa604b6a932da3
Andrea Lerose <a.lerose@itispa.com>
2026-04-08 10:43:28 +0200

    completamento esercizio

--- main.js
+++ main.js
@@ -6,3 +6,8 @@ let e = 15
 
 let somma = a + b + c + d + e;
 console.log('La somma tra i numeri equivale a: ' + (somma));
+
+let media = somma / 5;
+console.log('La media equivale a: ' + (media));
+
+console.log(`La somma tra i numeri equivale a ${somma} , mentre la media equivale a ${media}`);
